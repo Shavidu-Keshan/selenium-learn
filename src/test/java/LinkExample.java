4caac39fb51d42d2d35ca271e08c94d21d3d7bac
Shavidu Keshan <shavidu4321@gmail.com>
2024-08-11 10:20:20 +0530

    Complete link example part

--- src/test/java/LinkExample.java
+++ src/test/java/LinkExample.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class LinkExample {
     WebDriver driver;
     @BeforeMethod
@@ -37,9 +39,23 @@ public class LinkExample {
         }else {
             System.out.println("Not broken");
         }
+        driver.navigate().back();
+
+        //04)Duplicate Link
+        WebElement homeLink1 = driver.findElement(By.linkText("Go to Dashboard"));
+        homeLink1.click();
+        driver.navigate().back();
 
         //05) Count page Links
-        //06) count layout Links
-    }
 
-}
+        List<WebElement> countfullpageLinks = driver.findElements(By.tagName("a"));
+        int pageLinkCount = countfullpageLinks.size();
+        System.out.println("Count of full page Links :"+pageLinkCount);
+
+
+        //06) count layout Links
+        WebElement layoutElement = driver.findElement(By.className("layout-main-content")); // Corrected variable name
+        List<WebElement> countoflayoutLinks = layoutElement.findElements(By.tagName("a"));
+        System.out.println("Count of layout links: " + countoflayoutLinks.size()); // Print the size of the list
+
+}};
