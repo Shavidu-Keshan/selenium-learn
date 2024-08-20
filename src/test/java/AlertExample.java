a02158c8667f108d8c5fc4968a56ef6cdaf9cae3
Shavidu Keshan <shavidu4321@gmail.com>
2024-08-20 08:01:09 +0530

    do the test alert

--- src/test/java/AlertExample.java
+++ src/test/java/AlertExample.java
@@ -1,6 +1,10 @@
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class AlertExample {
     WebDriver driver;
@@ -9,5 +13,19 @@ public class AlertExample {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.leafground.com/alert.xhtml;jsessionid=node0fdzpuf8nuk8npivgy5j21oh65628256.node0");
+    }
+    @Test
+    public void alertTest(){
+        //1)simple diolog
+        WebElement alertbox = driver.findElement(By.id("j_idt88:j_idt91"));
+        alertbox.click();
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+
+        //2)alert confrirm dialog
+        WebElement confirmBox = driver.findElement(By.id("j_idt88:j_idt93"));
+        confirmBox.click();
+        Alert alert1= driver.switchTo().alert();
+        alert1.dismiss();;
     }
 }
